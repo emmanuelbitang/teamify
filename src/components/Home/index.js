ffbd2cbc315791afab82de3655334876f861e261
emmanuelbitang <emmanuelbitang@gmail.com>
2021-06-28 00:34:25 +0800

    change import quotes to single

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,20 +1,21 @@
 /* eslint-disable */
-import React, { useState, useEffect } from "react";
-import Member from "../Members";
-import axios from "axios";
-import LoadSpinner from "../LoadSpinner";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-import "./Home.css";
+import Member from '../Members';
+import LoadSpinner from '../LoadSpinner';
+
+import './Home.css';
 
 function Home() {
   const [members, setMembers] = useState([]);
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState('');
   const [isLoaded, setIsLoaded] = useState(true);
 
   useEffect(() => {
     const ac = new AbortController();
     axios
-      .get("https://jsonplaceholder.typicode.com/users")
+      .get('https://jsonplaceholder.typicode.com/users')
       .then((res) => {
         setMembers(res.data);
         setIsLoaded(false);
@@ -30,7 +31,7 @@ function Home() {
   const filteredMembers = members.filter((member) => {
     return Object.keys(member).some(
       (key) =>
-        (typeof member[key] === "string" &&
+        (typeof member[key] === 'string' &&
           member[key].toLowerCase().includes(search.toLowerCase())) ||
         !search
     );
@@ -38,28 +39,28 @@ function Home() {
 
   return (
     <div>
-      <div className='topnav'>
-        <a href='.'>Teamify</a>
-        <div className='search-container'>
+      <div className="topnav">
+        <a href=".">Teamify</a>
+        <div className="search-container">
           <form>
             <input
-              type='text'
-              placeholder='Search via name or email...'
-              name='search'
+              type="text"
+              placeholder="Search via name or email..."
+              name="search"
               onChange={handleChange}
-              title='search'
-              data-testid='search-textbox'
+              title="search"
+              data-testid="search-textbox"
             />
           </form>
         </div>
       </div>
 
-      <h1 className='title'>Team Members</h1>
+      <h1 className="title">Team Members</h1>
 
       {isLoaded ? (
         <LoadSpinner />
       ) : filteredMembers.length > 0 ? (
-        <div className='cards'>
+        <div className="cards">
           {filteredMembers.map((team) => {
             return (
               <Member
@@ -79,8 +80,9 @@ function Home() {
           })}
         </div>
       ) : (
-        <div className='no-result' data-testid='no-result'>
-          No results found for <b>{search}</b>
+        <div className="no-result" data-testid="no-result">
+          No results found for
+          <b>{search}</b>
         </div>
       )}
     </div>
--- src/components/Members/index.js
+++ src/components/Members/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
-import React from "react";
-import "./Members.css";
+import React from 'react';
+import './Members.css';
 
 const Member = ({
   name,
@@ -15,33 +15,33 @@ const Member = ({
   company,
 }) => {
   return (
-    <div className='cards'>
-      <div className='card'>
+    <div className="cards">
+      <div className="card">
         <img
-          src='https://qph.fs.quoracdn.net/main-qimg-2b21b9dd05c757fe30231fac65b504dd'
-          alt='Avatar'
+          src="https://qph.fs.quoracdn.net/main-qimg-2b21b9dd05c757fe30231fac65b504dd"
+          alt="Avatar"
         />
-        <div className='container'>
-          <ul className='details'>
+        <div className="container">
+          <ul className="details">
             <li>
-              <h1 data-testid='name'>{name}</h1>
+              <h1 data-testid="name">{name}</h1>
             </li>
             <li>Username: {username}</li>
             <li>
-              Website:{" "}
-              <a href={"http://" + website} target='_blank' rel='noreferrer'>
+              Website:{' '}
+              <a href={'http://' + website} target="_blank" rel="noreferrer">
                 {website}
               </a>
             </li>
             <li>
-              Email:{" "}
-              <a href={"mailto:" + email} data-testid='email-link'>
+              Email:{' '}
+              <a href={'mailto:' + email} data-testid="email-link">
                 {email}
               </a>
             </li>
             <li>
-              Phone Number:{" "}
-              <a href={"tel:" + phone} data-testid='phone-link'>
+              Phone Number:{' '}
+              <a href={'tel:' + phone} data-testid="phone-link">
                 {phone}
               </a>
             </li>
